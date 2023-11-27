216aba785985ba2d757e9823798deeae892c4c4a
Divyesh Vishwakarma <divyesh1099@gmail.com>
2023-11-27 16:42:16 +0530

    Add Notes Feature

--- cliniskills/note/models.py
+++ cliniskills/note/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    # Title of the note
+    title = models.CharField(max_length=255)
+
+    # The PDF file
+    file = models.FileField(upload_to='notes/')
+
+    # Upload date
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    # Optional description
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def filename(self):
+        return self.file.name.rsplit('/', 1)[-1]
--- cliniskills/note/views.py
+++ cliniskills/note/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+from .models import *
+from django.db.models import Q
+import datetime
 # Create your views here.
 def index(request):
-    return render(request, 'note/index.html')
+    notes = Note.objects.all().order_by('-upload_date')
+    # Handling the search query
+    query = request.GET.get('q')
+    if query:
+        # Attempt to parse the query as a date
+        try:
+            query_date = datetime.datetime.strptime(query, '%Y-%m-%d').date()
+            date_filter = Q(upload_date__date=query_date)
+        except ValueError:
+            # If the query is not a valid date, skip date filtering
+            date_filter = Q()
+
+        notes = notes.filter(
+            Q(title__icontains=query) | 
+            Q(description__icontains=query) |
+            date_filter
+        )
+
+    context={
+        'notes': notes
+    }
+    return render(request, 'note/index.html', context)
