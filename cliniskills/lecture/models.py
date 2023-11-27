1eea87f8046f993260f96850b100b95e6d81f73e
Divyesh Vishwakarma <divyesh1099@gmail.com>
2023-11-27 17:00:20 +0530

    Add Lecture Feature

--- cliniskills/lecture/models.py
+++ cliniskills/lecture/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Lecture(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.DateTimeField()
+    meet_link = models.URLField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def is_upcoming(self):
+        return timezone.now() < self.date
+
+    @property
+    def has_passed(self):
+        return timezone.now() > self.date
--- cliniskills/lecture/views.py
+++ cliniskills/lecture/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from .models import *
+from django.db.models import Q
+from django.utils import timezone
+import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, 'lecture/index.html')
+    lectures = Lecture.objects.all().order_by('-date')  # Ordering lectures by date
+
+    # Handling the search query
+    query = request.GET.get('q')
+    if query:
+        # Attempt to parse the query as a date
+        try:
+            query_date = datetime.datetime.strptime(query, '%Y-%m-%d').date()
+            date_filter = Q(created_at__date=query_date)
+        except ValueError:
+            # If the query is not a valid date, consider it a text search
+            date_filter = Q(title__icontains=query) | Q(description__icontains=query)
+
+        lectures = lectures.filter(date_filter)
+
+    # Pass current time to the template for comparison
+    current_time = timezone.now()
+    context = {
+        'lectures': lectures,
+        'current_time': current_time
+    }
+    return render(request, 'lecture/index.html', context)
